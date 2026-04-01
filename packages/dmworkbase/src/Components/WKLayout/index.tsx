fcc991161ff84333157f85e06fa020c3b05b7778
architect <architect-7bd2c471adb3@users.noreply.github.com>
2026-04-01 11:36:45 +0800

    fix(mobile): wk-layout-tab position:fixed on small screens so content-left takes full width; content-right z-index:10 covers tab when conversation open

--- packages/dmworkbase/src/Components/WKLayout/index.tsx
+++ packages/dmworkbase/src/Components/WKLayout/index.tsx
@@ -50,14 +50,13 @@ export class WKLayout extends Component<WKLayoutProps>{
 
     render() {
         const { onRenderTab, contentLeft,contentRight,onLeftContext,onRightContext } = this.props
-        const hasConversationOpen = this.rightContext?.viewCount() > 0
-        return <div className={classNames("wk-layout", hasConversationOpen ? "wk-layout-conversation-open" : undefined)}>
+        return <div className="wk-layout">
             <div className="wk-layout-tab">
                 {
                     onRenderTab && onRenderTab(window.innerWidth <= smallScreenWidth ? ScreenSize.small : ScreenSize.normal)
                 }
             </div>
-            <div className={classNames("wk-layout-content", hasConversationOpen ? "wk-layout-open" : undefined)}>
+            <div className={classNames("wk-layout-content", this.rightContext?.viewCount() > 0 ? "wk-layout-open" : undefined)}>
                 <div className="wk-layout-content-left">
                     <WKViewQueue onContext={(context) => {
                         if(onLeftContext) {
